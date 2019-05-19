41a7f428f78a0d8985acd15b17d8ee8249254c94
drimoch <davidrimoh@gmail.com>
2019-05-19 04:19:29 +0300

    Initial structure

---
+++ Garage/Garage/Vehicle.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ex3.GarageLogic
+{
+    class Vehicle
+    {
+        // Members
+        private string m_LicenseNumber;
+        private string m_ModelName;
+        private float m_CurrentEnergy;
+        private float m_MaxEnergy;
+        private float m_CurrentEnergyPercent;
+        public float CurrentEnergy
+        {
+            set
+            {
+                if (value + m_CurrentEnergy > m_MaxEnergy)
+                {
+                    //throw exception
+                }
+            }
+        }
+        public float CurrentEnergyPercent
+        {
+            get
+            {
+                return (m_CurrentEnergy / m_MaxEnergy) * 100;
+            }
+        }
+
+        public string LicenseNumber
+
+        {
+            get
+            {
+                return m_LicenseNumber;
+            }
+            set
+            {
+                m_LicenseNumber = value;
+            }
+        }
+    }
+}
